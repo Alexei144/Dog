c0d3e107385082db15b6e44c9fde46adf97773fe
Alexei144 <107850830+Alexei144@users.noreply.github.com>
2023-05-30 07:45:37 +0300

    Task 2.1.4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,18 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    private Timer timer;
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     public void whatAnimalSay() {
@@ -16,4 +22,9 @@ public class AnimalsCage {
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
     }
+
+    public Timer getTimer() {
+        return timer;
+    }
+
 }
